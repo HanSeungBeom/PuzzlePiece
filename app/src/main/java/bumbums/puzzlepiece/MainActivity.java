074716f1c966f1bdc7f0a6610d4615e59a8c6290
bumbums <aronix@naver.com>
2017-02-07 14:36:17 +0900

    메인액티비티 간단한 UI구성 및 친구추가 기능.

--- app/src/main/java/bumbums/puzzlepiece/MainActivity.java
+++ app/src/main/java/bumbums/puzzlepiece/MainActivity.java
@@ -1,7 +1,12 @@
 package bumbums.puzzlepiece;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addFriend(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(intent, 0);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK)
+        {
+            Cursor cursor = getContentResolver().query(data.getData(),
+                    new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
+            cursor.moveToFirst();
+            //name = cursor.getString(0);        //0은 이름을 얻어옵니다.
+            //number = cursor.getString(1);   //1은 번호를 받아옵니다.
+            Log.d("###","name="+cursor.getString(0)+"/number="+cursor.getString(1)) ;
+            cursor.close();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
 }
---
+++ app/src/main/java/bumbums/puzzlepiece/Task.java
@@ -0,0 +1,13 @@
+package bumbums.puzzlepiece;
+
+import android.content.Context;
+
+/**
+ * Created by 한승범 on 2017-02-07.
+ */
+
+public class Task {
+    public static void addFriend(Context context){
+
+    }
+}
