5196dfd87680bdbbb0f4d036a5ddae9669c2d4fe
aronix <aronix@naver.com>
2017-02-23 02:09:28 +0900

    splash 추가

---
+++ app/src/main/java/bumbums/puzzlepiece/ui/Splash.java
@@ -0,0 +1,26 @@
+package bumbums.puzzlepiece.ui;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bumbums.puzzlepiece.R;
+
+public class Splash extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
